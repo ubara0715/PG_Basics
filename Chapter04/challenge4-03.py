636e4c8b85851eeb768bf29a636ed67611e55fa7
itc-c24028 <c24028@std.it-college.ac.jp>
2025-05-28 15:43:59 +0900

    修正点あり、修正済み

--- Chapter04/challenge4-03.py
+++ Chapter04/challenge4-03.py
@@ -1,3 +1,5 @@
+def f(x,y,z,a = 3,b = 8):
+    return x + (y + z) + a * b
 '''
 Return x + (y + z) + a * b
 :param x: int.
@@ -7,7 +9,5 @@ Return x + (y + z) + a * b
 ;param b: int. initial value 8.
 :return: int sum of y and z. next, int product of a and b. next, int sum of x and (sum of)yz. next, int sum of (sum of)xyz and (product of)ab.
 '''
-def f(x,y,z,a = 3,b = 8):
-    return x + (y + z) + a * b
 
 print(f(4,1,2))
--- Chapter04/challenge4-04.py
+++ Chapter04/challenge4-04.py
@@ -1,15 +1,20 @@
+def f(x):
+    return x/2
 '''
 Returns x/2
 :param x: int.
 :return: xを2で割った結果
 '''
-def f(x):
-    return x/2
 
 g = f(24)
 print(g)
 
 def h(y):
     return y*4
+'''
+Return y*4
+:param y: int.
+:return: yに4をかけた結果
+'''
 
 print(h(g))
